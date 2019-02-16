a8bc6d882e45e543752c332b5adf2a74d9be4ebd
MichaelCapo23 <123pie23@gmail.com>
2019-02-16 15:15:38 -0800

    started on jump problem needs more work to finish

--- jump-problem/main.js
+++ jump-problem/main.js
@@ -3,15 +3,29 @@
 const jumpArr = [2,3,1,1,4];
 
 function getToEnd(jumpArr) {
+    console.log(jumpArr);
     for(let index = 0; index < jumpArr.length; index++) {
-        let lastNumber = jumpArr[jumpArr.length];
-        let currentNumber = jumpArr[index];
-        let movesPerJump = (jumpArr.length - index+1) / lastNumber;
-        if(movesPerJump % 2 !== 0) {
-            for(let jumpNumber = 0; jumpNumber < (jumpArr.length - index+1); jumpNumber++) {
-                let temp = jumpArr[index+1];
-
+        let lastNumber = jumpArr[jumpArr.length-1];
+        let currentNumber = jumpArr[0];
+        let movesPerJump = (jumpArr.length - 1) / lastNumber;
+        if(movesPerJump % 1 === 0) {
+            for(let jumpNumber = 0; jumpNumber < ((jumpArr.length-1) / movesPerJump); jumpNumber++) {
+                debugger;
+                let temp = jumpArr[(jumpNumber.indexOf(currentNumber).)+movesPerJump];
+                jumpArr[jumpNumber+movesPerJump] = currentNumber;
+                jumpArr[jumpNumber] = temp;
+                console.log(jumpArr);
+            }
+        } else {
+            let indexesToJump = Math.ceil(movesPerJump);
+            for(let jumpNumber = 0; jumpNumber < ((jumpArr.length-1) / movesPerJump); jumpNumber++) {
+                let temp = jumpArr[(jumpNumber.indexOf(currentNumber))+indexesToJump];
+                jumpArr[jumpNumber+indexesToJump] = currentNumber;
+                jumpArr[jumpNumber] = temp;
+                console.log(jumpArr);
             }
         }
     }
 }
+
+getToEnd(jumpArr);
