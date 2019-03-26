99a8e77cafe3d8052dea6dfbc1fd65fc1732311b
MichaelCapo23 <123pie23@gmail.com>
2019-03-25 19:31:15 -0700

    started on function

---
+++ main.js
@@ -0,0 +1,22 @@
+//Practicing recursive functions. Get a rolling sum of all values in an array, some indexes could bve strings/arrays.
+let arr = [2, 7, 4, [5, 7, 5], "hey", [4, 3, 2], 1, "heyo"];
+
+function arraySum(arr) {
+    debugger;
+    let sum = 0;
+    function itemSum(arr) {
+        arr.forEach(function (item) {
+            if(typeof item === 'array') {
+                sum += itemSum(item);
+            } else if (typeof item === 'number') {
+                sum += item;
+            }
+        })
+        let sumItem = itemSum(item);
+        return sumItem
+    }
+    return sum;
+}
+
+let total = arraySum(arr);
+console.log(total);
